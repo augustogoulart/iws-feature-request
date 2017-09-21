956409fa14441e45c514f037884710716162aafc
Augusto Goulart <augusto@goulart.me>
2017-09-21 13:31:35 -0300

    Ajax integration for post submit (#9)
    * Feature request row
    
    * Icons by Font Awesome
    
    * Product area name alongside client name
    
    * Feature request form submission
    
    * Delete requests
    
    * Ajax integration for post submit
    
    * Resolve git conflicts

--- iws_feature_request/static/js/app.js
+++ iws_feature_request/static/js/app.js
@@ -4,28 +4,57 @@ function FeatureRequestViewModel() {
     self.requests = ko.observable();
     self.requestDetail = ko.observable();
 
-    self.getRequests = function(){
+    self.submitSuccess = ko.observable();
+
+    self.getRequests = function () {
         self.requestDetail(null);
-        $.get('/api/requests/', self.requests );
+        $.get('/api/requests/', self.requests);
     };
 
     self.getRequestDetail = function (request) {
         self.requests(null);
-        $.get('/api/requests/'+request.id, self.requestDetail)
+        $.get('/api/requests/' + request.id, self.requestDetail)
+
+        
+          
     };
 
     self.deleteRequest = function (message) {
         $.ajax({
-            url:'/api/requests/'+message.id,
+            url: '/api/requests/' + message.id,
             type: 'DELETE',
-            data: {message:message},
-            success: function() {
+            data: {message: message},
+            success: function () {
                 self.getRequests();
-             }
-        })
+            }
+        });          
 
     };
 
+
+    self.postRequest = function () {
+        $.ajax({
+            url: '/api/requests/',
+            type: 'POST',
+            contentType: "application/json",
+            accepts: "application/json",
+            dataType: 'json',
+            data: JSON.stringify({
+                title: 'Add more buttons to the dashboard',
+                client: 'Britecore',
+                description: 'At this point we have very few buttons. People love buttons, lets add more',
+                target_date: '2017-10-10'
+            }),
+            success: function () {
+                self.submitSuccess(true)
+
+            },
+            error: function (jqXHR) {
+                console.log("POST error: " + jqXHR.status);
+            }
+        });
+
+    };
     self.getRequests();
 }
 
