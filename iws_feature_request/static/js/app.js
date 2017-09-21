1ebd17fa5deb498343451104f9d7c6c1413b594f
Augusto Goulart <augusto@goulart.me>
2017-09-21 08:11:28 -0300

    Detail section with 'go back' button

--- iws_feature_request/static/js/app.js
+++ iws_feature_request/static/js/app.js
@@ -4,16 +4,17 @@ function FeatureRequestViewModel() {
     self.requests = ko.observable();
     self.requestDetail = ko.observable();
 
-    self.getRequests = (function(){
+    self.getRequests = function(){
         self.requestDetail(null);
         $.get('/api/requests/', self.requests );
-    }());
+    };
 
     self.getRequestDetail = function (request) {
         self.requests(null);
         $.get('/api/requests/'+request.id, self.requestDetail)
     }
 
+    self.getRequests();
 }
 
 ko.applyBindings(new FeatureRequestViewModel());
